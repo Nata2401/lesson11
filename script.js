236e01c3bbe6aaf7b762f37981102d9bd4913878
Nata2401 <kuzyan.rd@yandex.ru>
2021-12-09 18:44:34 +0300

    Выполнено задание 11

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-'use ctrict';
+'use strict';
+const btn = document.querySelector('#btn');
+const btnCircle = document.querySelector('#e_btn');
+const square = document.querySelector('#square');
+const input = document.querySelector('#text');
+const range = document.querySelector('#range');
+const circle = document.querySelector('#circle');
+const span = document.querySelector('#range-span');
+
+btn.addEventListener('click', function () {
+  square.style.backgroundColor = document.querySelector("input[type ='text']").value;
+});
+
+btnCircle.style.display = 'none';
+
+range.addEventListener('input', function (event) {
+  span.textContent = event.target.value;
+  circle.style.width = range.value + '%';
+  circle.style.height = range.value + '%';
+});
 
 
 
